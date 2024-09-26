1b86c32f9257a04bbbb330b7159a69169344331a
Admin <admin@Admins-MacBook-Air.local>
2024-09-26 16:40:51 -0400

    Completed till all category pages- footer,Item,Navbar,NewCollection,Offers,ShopContext,ShopCategory

--- src/Components/Nevbar/Navbar.jsx
+++ src/Components/Nevbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import './Nevbar.css'
-const Nevbar = () => {
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    const[menu,setMenu] = useState("Shop");
     return (
-        <><div className='navbar'></div><h1>hello</h1></>
+        <><div className='navbar'>
+             <div className='nav-logo'>
+                <img src={logo} alt="" />
+                <p>SHOPPER</p>
+
+             </div>
+              <ul className='nav-menu'>
+                <li onClick = {()=> {setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick = {()=> {setMenu("mens")}}><Link to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+                <li onClick = {()=> {setMenu("womens")}}><Link to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+                <li onClick = {()=> {setMenu("kids")}}><Link to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+              </ul>
+              <div className="nav-login-cart">
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+                <div className="nav-cart-count">0</div>
+              </div>
+           </div></>
     )
 }
 
-export default Nevbar
+export default Navbar
